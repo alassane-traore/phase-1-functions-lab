5242c934d99b39cf7af1db06400b016c086663d1
alassane-traore <alassanet076@gmail.com>
2022-08-22 04:08:22 +0300

    Completed commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (pickupBlok){
+    const Hq = 42 
+    return pickupBlok <= Hq ? Hq-pickupBlok : pickupBlok-Hq
+    }
+
+   function distanceFromHqInFeet(feet){
+    return  distanceFromHqInBlocks (feet)*264
+    }
+
+    function distanceTravelledInFeet(from, to){
+        return from < to ? (to-from)*264:
+        from > to ? (from-to)*264: undefined
+     }
+
+     function calculatesFarePrice (start, destination){
+        return distanceTravelledInFeet(start,destination)<= 400 ? 0 :
+        distanceTravelledInFeet(start, destination) > 400 && 
+        distanceTravelledInFeet(start,destination)<=2000 ? 
+        (distanceTravelledInFeet(start, destination) - 400)*0.02 :
+        distanceTravelledInFeet(start, destination) > 2000 &&
+        distanceTravelledInFeet(start, destination) <= 2500 ? 25 :
+        distanceTravelledInFeet(start, destination) > 2500 ? 'cannot travel that far': undefined;
+     }
+
+     
